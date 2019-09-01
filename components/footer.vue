a312e48a495c551a61eb831065cd221b1fdc4416
carrot cat <carrot cat@gmail.com>
2019-09-01 20:29:54 +0800

    02-公共头部的添加

---
+++ components/footer.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="footer">
+    <div class="main">
+      <el-row>
+        <el-col :span="6" :offset="1">
+          <h5>闲云旅游旅游网</h5>
+          <p>上亿旅行者共同打造的"旅行神器"</p>
+          <p>
+            <span>60,000</span> 多个全球旅游目的地
+          </p>
+          <p>
+            <span>600,000</span> 个细分目的地新玩法
+          </p>
+          <p>
+            <span>760,000,000</span> 次攻略下载
+          </p>
+          <p>
+            <span>38,000</span> 家旅游产品供应商
+          </p>
+        </el-col>
+        <el-col :span="5">
+          <h5>关于我们</h5>
+          <p>隐私政策 商标声明</p>
+          <p>服务协议 游记协议</p>
+          <p>商城平台服务协议</p>
+          <p>网络信息侵权通知指引</p>
+          <p>闲云旅游旅游网服务监督员</p>
+          <p>网站地图加入闲云旅游</p>
+        </el-col>
+        <el-col :span="5">
+          <h5>旅行服务</h5>
+          <p>旅游攻略 酒店预订</p>
+          <p>旅游特价 国际租车</p>
+          <p>旅游问答 旅游保险</p>
+          <p>旅游指南 订火车票</p>
+          <p>旅游资讯 APP下载</p>
+        </el-col>
+        <el-col :span="6" class="scan">
+          <p>
+            <img src="http://157.122.54.189:9093/images/1556522965.png" alt />
+          </p>关注我们
+        </el-col>
+      </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scope>
+.footer {
+  width: 100%;
+  background-color: #333;
+  .main {
+    width: 1000px;
+    height: 329px;
+    margin: 0 auto;
+    padding-top: 30px;
+      color: #ccc;
+    p {
+      font-size: 12px;
+        color: #ccc;
+      span {
+        color: orange;
+      }
+    }
+    img{
+      width: 140px;
+      height: 140px;
+    }
+  }
+}
+</style>
---
+++ components/header.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container">
+    <el-row type="flex" class="main" justify="space-between">
+      <a href class="logo">
+        <img src="http://157.122.54.189:9093/images/logo.jpg" alt />
+      </a>
+      <el-row type="flex" class="nav">
+        <nuxt-link to="/">首页</nuxt-link>
+        <nuxt-link to="/post">旅游攻略</nuxt-link>
+        <nuxt-link to="/hotel">酒店</nuxt-link>
+        <nuxt-link to="/air">国内机票</nuxt-link>
+      </el-row>
+      <div class="login">
+        <nuxt-link to="/user/login">登录 / 注册</nuxt-link>
+      </div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scope>
+.container {
+  width: 100%;
+  height: 60px;
+  line-height: 60px;
+  border-bottom: 1px #ddd solid;
+  box-shadow: 0 2px 2px #ddd;
+  .main {
+    width: 1000px;
+    margin: 0 auto;
+    .logo {
+      padding-top: 9px;
+      img {
+        width: 156px;
+        height: 42px;
+        display: block;
+      }
+    }
+    .nav {
+      flex: 1;
+      margin: 0 20px;
+      a {
+        display: block;
+        padding: 0 20px;
+        height: 60px;
+        box-sizing: border-box;
+        &:hover {
+          color: #409eff;
+          border-bottom: 5px solid #409eff;
+        }
+      }
+      .nuxt-link-exact-active {
+        color: #fff;
+        background-color: #409eff;
+          &:hover {
+          color: #fff;
+        }
+      }
+    }
+    .login{
+        a {
+          &:hover{
+          color: #409eff;
+          }
+        }
+    }
+  }
+}
+</style>
