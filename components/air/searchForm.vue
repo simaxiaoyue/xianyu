76e311f1045984fe8f3e31e3b4178efbf26d2b5c
carrot cat <carrot cat@gmail.com>
2019-09-05 16:13:02 +0800

    06-机票首页布局和搜索(上)

--- components/air/searchForm.vue
+++ components/air/searchForm.vue
@@ -5,23 +5,51 @@
       <span
         v-for="(item, index) in tabs"
         :key="index"
-        @click="handleSearchTab(item, index)"
+        @click="handleSearchTab(index)"
         :class="{active: index === currentTab}"
       >
         <i :class="item.icon"></i>
         {{item.name}}
       </span>
     </el-row>
+
     <el-form class="search-form-content" ref="form" label-width="80px">
       <el-form-item label="出发城市">
-        <!-- <el-autocomplete
-          class="inline-input"
-          v-model="state1"
-          :fetch-suggestions="querySearch"
-          placeholder="请输入内容"
-          @select="handleSelect"
-        ></el-autocomplete> -->
+        <!-- fetch-suggestions 返回输入建议的方法 -->
+        <!-- select 点击选中建议项时触发 -->
+        <el-autocomplete
+          :fetch-suggestions="queryDepartSearch"
+          placeholder="请搜索出发城市"
+          @select="handleDepartSelect"
+          class="el-autocomplete"
+          v-model="form.departCity"
+        ></el-autocomplete>
       </el-form-item>
+      <el-form-item label="到达城市">
+        <el-autocomplete
+          :fetch-suggestions="queryDestSearch"
+          placeholder="请搜索到达城市"
+          @select="handleDestSelect"
+          class="el-autocomplete"
+          v-model="form.destCity"
+        ></el-autocomplete>
+      </el-form-item>
+      <el-form-item label="出发时间">
+        <!-- change 用户确认选择日期时触发 -->
+        <el-date-picker
+          type="date"
+          placeholder="请选择日期"
+          style="width: 100%;"
+          @change="handleDate"
+          v-model="form.departDate"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item label>
+        <el-button style="width:100%;" type="primary" icon="el-icon-search" @click="handleSubmit">搜索</el-button>
+      </el-form-item>
+      <div class="reverse">
+        <span @click="handleReverse">换</span>
+      </div>
     </el-form>
   </div>
 </template>
@@ -34,11 +62,96 @@ export default {
         { icon: "iconfont icondancheng", name: "单程" },
         { icon: "iconfont iconshuangxiang", name: "往返" }
       ],
-      currentTab: 0
+      currentTab: 0,
+      form: {
+        departCity: "",
+        departCode: "",
+        destCity: "",
+        destCode: "",
+        departDate: ""
+      }
     };
   },
   methods: {
     // tab切换时触发
+    handleSearchTab(index) {
+      if (index === 1) {
+      }
+    },
+
+    // 出发城市输入框获得焦点时触发
+    // value 是选中的值，cb是回调函数，接收要展示的列表
+    queryDepartSearch(value, cb) {
+      if (!value) {
+        cb([]);
+        return;
+      }
+      //发送请求,获取机票城市数据
+      this.$axios({
+        url: "/airs/city",
+        params: { name: value }
+      }).then(res => {
+        const { data } = res.data;
+        const newData = [];
+        data.forEach(e => {
+          e.value = e.name.replace("市", "");
+          newData.push(e);
+        });
+        // 默认选中第一个
+        this.form.departCity = newData[0].value;
+        this.form.departCode = newData[0].sort;
+        //显示到下拉列表中
+        cb(newData);
+      });
+    },
+
+    // 目标城市输入框获得焦点时触发
+    // value 是选中的值，cb是回调函数，接收要展示的列表
+    queryDestSearch(value, cb) {
+      if (!value) {
+        cb([]);
+        return;
+      }
+      //发送请求,获取机票城市数据
+      this.$axios({
+        url: "/airs/city",
+        params: { name: value }
+      }).then(res => {
+        const { data } = res.data;
+        const newData = [];
+        data.forEach(e => {
+          e.value = e.name.replace("市", "");
+          newData.push(e);
+        });
+        // 默认选中第一个
+        this.form.destCity = newData[0].value;
+        this.form.destCode = newData[0].sort;
+        //显示到下拉列表中
+        cb(newData);
+      });
+    },
+
+    // 出发城市下拉选择时触发
+    handleDepartSelect(item) {
+      console.log(item);
+      this.form.departCity = item.value;
+      this.form.departCode = item.sort;
+    },
+
+    // 目标城市下拉选择时触发
+    handleDestSelect(item) {
+      this.form.destCity = item.value;
+      this.form.destCode = item.sort;
+    },
+
+    // 确认选择日期时触发
+    handleDate(value) {},
+
+    // 触发和目标城市切换时触发
+    handleReverse() {},
+
+    // 提交表单是触发
+    handleSubmit() {}
   },
   mounted() {}
 };
@@ -76,6 +189,75 @@ export default {
 
     &:first-child {
       font-size: 16px;
+    }
+  }
+}
+
+.search-form-content {
+  padding: 15px 50px 15px 15px;
+  position: relative;
+
+  .el-autocomplete {
+    width: 100%;
+  }
+}
+
+.reverse {
+  position: absolute;
+  top: 35px;
+  right: 15px;
+
+  &:after,
+  &:before {
+    display: block;
+    content: "";
+    position: absolute;
+    left: -35px;
+    width: 25px;
+    height: 1px;
+    background: #ccc;
+  }
+
+  &:after {
+    top: 0;
+  }
+
+  &:before {
+    top: 60px;
+  }
+
+  span {
+    display: block;
+    position: absolute;
+    top: 20px;
+    right: 0;
+    font-size: 12px;
+    background: #999;
+    color: #fff;
+    width: 20px;
+    height: 20px;
+    line-height: 18px;
+    text-align: center;
+    border-radius: 2px;
+    cursor: pointer;
+
+    &:after,
+    &:before {
+      display: block;
+      content: "";
+      position: absolute;
+      left: 10px;
+      width: 1px;
+      height: 20px;
+      background: #ccc;
+    }
+
+    &:after {
+      top: -20px;
+    }
+
+    &:before {
+      top: 20px;
     }
   }
 }
