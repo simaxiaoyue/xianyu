62ad80e8711c40a47c523c68fc9a72c9951b8e96
carrot cat <carrot cat@gmail.com>
2019-09-05 16:44:08 +0800

    08-特价机票的展示

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -40,7 +40,21 @@
     </h2>
 
     <!-- 特价机票 -->
-    <div class="air-sale"></div>
+    <div class="air-sale">
+      <el-row type="flex" class="air-sale-pic" justify="space-between">
+        <el-col :span="6" v-for="(item, index) in sales" :key="index">
+          <nuxt-link
+            :to="`/air/flights?departCity=${item.departCity}&departCode=${item.departCode}&destCity=${item.destCity}&destCode=${item.destCode}&departDate=${item.departDate}`"
+          >
+            <img :src="item.cover" />
+            <el-row class="layer-bar" type="flex" justify="space-between">
+              <span>{{item.departCity}}-{{item.destCity}}</span>
+              <span>￥{{ item.price }}</span>
+            </el-row>
+          </nuxt-link>
+        </el-col>
+      </el-row>
+    </div>
   </section>
 </template>
 
@@ -48,8 +62,22 @@
 import SearchForm from "@/components/air/searchForm";
 
 export default {
-  components:{
+  data() {
+    return {
+      sales: []
+    };
+  },
+  components: {
     SearchForm
+  },
+  mounted() {
+    this.$axios({
+      url: "/airs/sale"
+    }).then(res => {
+     console.log(res.data);
+     const {data}=res.data
+      this.sales=data
+    });
   }
 };
 </script>
